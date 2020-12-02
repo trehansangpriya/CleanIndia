02ddb6268aaf5cb9cb5f6caf621138f73c45529f
Trehan Sangpriya <trehansangpriya@gmail.com>
2020-12-03 00:20:44 +0530

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,16 +3,16 @@ const tl2 = gsap.timeline({ defaults: { duration: 1.5, delay: 1 } })
 tl.to('.fixed-top', { opacity: 0, duration: 0 })
 tl.to('.btn-hero', { opacity: 0, duration: 0 })
 
-// tl.to('.text', {
-//     y: '0%', duration: .7
-// })
+tl.to('.text', {
+    y: '0%', duration: .7
+})
 
-// tl.to('.intro2', { x: '0%', duration: 1, delay: .7 })
+tl.to('.intro2', { x: '0%', duration: 1, delay: .7 })
 
-// tl.to('.text2', { y: '0%', duration: .8 })
+tl.to('.text2', { y: '0%', duration: .8 })
 
-// tl.to('.intro', { y: '-100%', duration: .5, delay: .5 })
-// tl.to('.intro2', { y: '-100%', duration: 1 })
+tl.to('.intro', { y: '-100%', duration: .5, delay: .5 })
+tl.to('.intro2', { y: '-100%', duration: 1 })
 tl.to('.fixed-top', { opacity: 1, duration: 1 })
 tl.from('.heading-hero', { opacity: 0, duration: .5 })
 tl.from('.hero-img', { y: "190%", opacity: 0, duration: 1.5 })
