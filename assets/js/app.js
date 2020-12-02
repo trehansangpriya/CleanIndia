a4435ca5da005fb993f19bce8e3c6ba4c540683f
Trehan Sangpriya <trehansangpriya@gmail.com>
2020-12-02 15:09:11 +0530

    update1

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+const tl = gsap.timeline({ default: { ease: 'power4' } })
+tl.to('.fixed-top', { opacity: 0, duration: 0 })
+
+tl.to('.text', {
+    y: '0%', duration: .7
+})
+
+tl.to('.intro2', { x: '0%', duration: 1, delay: 0.5 })
+
+tl.to('.text2', { y: '0%', duration: .7 })
+
+tl.to('.intro', { y: '-100%', duration: .5 })
+tl.to('.intro2', { y: '-100%', duration: 1 })
+tl.to('.fixed-top', { opacity: 1, duration: 1 })
