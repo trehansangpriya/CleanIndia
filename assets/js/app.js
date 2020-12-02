667e984c37a100f1b9445b4b24d0963568b7c842
Trehan Sangpriya <trehansangpriya@gmail.com>
2020-12-02 15:14:08 +0530

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,10 +5,10 @@ tl.to('.text', {
     y: '0%', duration: .7
 })
 
-tl.to('.intro2', { x: '0%', duration: 1, delay: 0.5 })
+tl.to('.intro2', { x: '0%', duration: 1, delay: .7 })
 
-tl.to('.text2', { y: '0%', duration: .7 })
+tl.to('.text2', { y: '0%', duration: .8 })
 
-tl.to('.intro', { y: '-100%', duration: .5 })
+tl.to('.intro', { y: '-100%', duration: .5, delay: .5 })
 tl.to('.intro2', { y: '-100%', duration: 1 })
 tl.to('.fixed-top', { opacity: 1, duration: 1 })
